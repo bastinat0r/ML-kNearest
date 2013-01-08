92adc136b81e017326c7902d91483fe9df4e1935
barthimaeus <barthimaeus@web.de>
2013-01-08 17:47:30 +0100

    innit

---
+++ kNearest/Naive.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * Naive.java
+ * created: Jan 8, 2013
+ *
+ */
+package kNearest;
+
+/**
+ * @author sebastian
+ *
+ */
+public class Naive implements Distance {
+
+	@Override
+	public double dist(String[] x, String[] y) {
+		double d = 0;
+		for(int i = 0; i < x.length && i < y.length; i++) {
+			if(! x[i].equalsIgnoreCase(y[i])) {
+				d++;
+			}
+		}
+		return d;
+	}
+
+}
